c4f10978dc4ae0a3dcfd9265f4207b9d0c6a966f
Ali Asker Memiş <61269823+1lirisist@users.noreply.github.com>
2021-04-16 22:14:44 +0300

    v0.3

--- sql/bloodbank.sql
+++ sql/bloodbank.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 10, 2021 at 07:51 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `bloodbank`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bloodinfo`
---
 
 CREATE TABLE `bloodinfo` (
   `bid` int(11) NOT NULL,
@@ -33,21 +8,11 @@ CREATE TABLE `bloodinfo` (
   `bg` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `bloodinfo`
---
-
 INSERT INTO `bloodinfo` (`bid`, `hid`, `bg`) VALUES
 (1, 1, 'B+'),
 (3, 1, 'O-'),
 (4, 2, 'AB-'),
 (5, 1, 'A+');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bloodrequest`
---
 
 CREATE TABLE `bloodrequest` (
   `reqid` int(11) NOT NULL,
@@ -56,12 +21,6 @@ CREATE TABLE `bloodrequest` (
   `bg` varchar(11) NOT NULL,
   `status` varchar(100) NOT NULL DEFAULT 'Bekliyor'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hospitals`
---
 
 CREATE TABLE `hospitals` (
   `id` int(11) NOT NULL,
@@ -72,19 +31,9 @@ CREATE TABLE `hospitals` (
   `hcity` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `hospitals`
---
-
 INSERT INTO `hospitals` (`id`, `hname`, `hemail`, `hpassword`, `hphone`, `hcity`) VALUES
 (1, 'Acıbadem Atakent ', 'acibadem@mail.com', 'acibadem34', '+90 (0212) 404 44 44', 'İstanbul'),
 (2, 'Medical Park Batıkent', 'medicalpark@mail.com', 'medicalpark06', '+90 (0312) 666 80 00', 'Ankara');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `receivers`
---
 
 CREATE TABLE `receivers` (
   `id` int(11) NOT NULL,
@@ -96,85 +45,38 @@ CREATE TABLE `receivers` (
   `rcity` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `receivers`
---
-
 INSERT INTO `receivers` (`id`, `rname`, `remail`, `rpassword`, `rphone`, `rbg`, `rcity`) VALUES
 (1, 'Ali Asker Memiş', 'aliasker@mail.com', 'aliasker34', '+90 539 345 6944', 'A+', 'İstanbul'),
 (2, 'deneme', 'deneme@mail.com', 'deneme', '+90 111 111 1111', 'AB+', 'Ankara');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `bloodinfo`
---
 ALTER TABLE `bloodinfo`
   ADD PRIMARY KEY (`bid`),
   ADD KEY `hid` (`hid`);
 
---
--- Indexes for table `bloodrequest`
---
 ALTER TABLE `bloodrequest`
   ADD PRIMARY KEY (`reqid`),
   ADD KEY `hid` (`hid`);
 
---
--- Indexes for table `hospitals`
---
 ALTER TABLE `hospitals`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `hemail` (`hemail`);
 
---
--- Indexes for table `receivers`
---
 ALTER TABLE `receivers`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `remail` (`remail`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `bloodinfo`
---
 ALTER TABLE `bloodinfo`
   MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
---
--- AUTO_INCREMENT for table `bloodrequest`
---
 ALTER TABLE `bloodrequest`
   MODIFY `reqid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
---
--- AUTO_INCREMENT for table `hospitals`
---
 ALTER TABLE `hospitals`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `receivers`
---
 ALTER TABLE `receivers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `bloodinfo`
---
 ALTER TABLE `bloodinfo`
   ADD CONSTRAINT `bloodinfo_ibfk_1` FOREIGN KEY (`hid`) REFERENCES `hospitals` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
